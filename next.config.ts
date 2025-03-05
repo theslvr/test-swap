fb7c957e883b7e9e6a75cf7d42d230cdd86147d5
slvr <slvrslvr@slvr.slvr>
2025-03-05 12:14:49 +0300

    4

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   assetPrefix: '/test-swap/',
-  basePath: '/test-swap/',
+  basePath: '/test-swap',
   /* config options here */
 };
 
